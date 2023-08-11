38175ce0d50a7a0eae4377d8e4e4cb45ea64a4ca
DhrumilGohil <dhrumilgohil17@gmail.com>
2023-08-11 14:42:59 +0000

    small changes

--- restore_latest_duplicity.sh
+++ restore_latest_duplicity.sh
@@ -8,16 +8,27 @@ while true; do
         break
     fi
 echo "$sitename"
+
+
+
 EE_DIR_PATH="/opt/easyengine/sites/$sitename"
-echo "Checking..."
 DRIVE_DIR=gdocs://dhrumil.gohil@rtcamp.com/duplicity
+GPG_ID=2349166A7DE7FCC0537CEDA47A335E00B8E26D91
+
+echo "Checking..."
 if [ -d $EE_DIR_PATH ]; then
         echo "$sitename site found"
 	echo "Backup in progress..."
-GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /code/htdocs/ "${DRIVE_DIR}/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs
-
-GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /db  "${DRIVE_DIR}/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs/db_file
-
+	if [ -d ${EE_DIR_PATH}/app/htdocs ]; then
+	       	rm -r ${EE_DIR_PATH}/app/htdocsi
+	fi
+	GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /code/htdocs/ "${DRIVE_DIR}/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs
+	
+	GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /db/  "${DRIVE_DIR}/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs/db_backup
+	
+	echo "Import Database..."
+	ee shell $sitename --command='wp db import db_backup/db_backup.sql'
+	rm -r ${EE_DIR_PATH}/app/htdocs/db_backup
 else 
 	echo "Opps! Site not found"
 fi
