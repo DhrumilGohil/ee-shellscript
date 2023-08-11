929d1ec41dfa66afd9fc0f252c951eee67584c84
DhrumilGohil <dhrumilgohil17@gmail.com>
2023-08-11 10:26:35 +0000

    add final script

--- drive_wp_backup_duplicity.sh
+++ drive_wp_backup_duplicity.sh
@@ -10,6 +10,18 @@ for site in $(ls $EE_DIR); do
                 echo "Starting WordPress EasyEngine backup for ${site}"
                 echo "---------------------------------------------"
 		
-		GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity --encrypt-key 2349166A7DE7FCC0537CEDA47A335E00B8E26D91  ${EE_DIR}/${site}/app/htdocs gdocs://dhrumil.gohil@rtcamp.com/duplicity/${site}_backup
+		code_dir=${BAKUP_DIR}/${site}/code
+		db_dir=${BAKUP_DIR}/${site}/db
+		mkdir -p $code_dir
+		mkdir -p $db_dir
+
+		cp -r ${EE_DIR}/${site}/app/htdocs $code_dir
+
+		# export db
+                ee shell $site --command='wp db export db_backup.sql'
+
+		cp ${EE_DIR}/${site}/app/htdocs/db_backup.sql $db_dir
+			
+		GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity --encrypt-key 2349166A7DE7FCC0537CEDA47A335E00B8E26D91  ${BAKUP_DIR}/${site}/ gdocs://dhrumil.gohil@rtcamp.com/duplicity/${site}_backup
         fi
 done
