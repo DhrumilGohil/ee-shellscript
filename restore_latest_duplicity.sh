64a8886b2b4fd2e49862ca847a8292918ea1e616
DhrumilGohil <dhrumilgohil17@gmail.com>
2023-08-11 13:24:01 +0000

    small change

--- restore_latest_duplicity.sh
+++ restore_latest_duplicity.sh
@@ -10,13 +10,13 @@ while true; do
 echo "$sitename"
 EE_DIR_PATH="/opt/easyengine/sites/$sitename"
 echo "Checking..."
-DRIVE_ID=gdocs://dhrumil.gohil@rtcamp.com/duplicity
+DRIVE_DIR=gdocs://dhrumil.gohil@rtcamp.com/duplicity
 if [ -d $EE_DIR_PATH ]; then
         echo "$sitename site found"
 	echo "Backup in progress..."
-GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /code/htdocs/ "gdocs://dhrumil.gohil@rtcamp.com/duplicity/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs
+GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /code/htdocs/ "${DRIVE_DIR}/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs
 
-GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /db  "gdocs://dhrumil.gohil@rtcamp.com/duplicity/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs/db_file
+GOOGLE_DRIVE_SETTINGS=~/.duplicity/credentials duplicity restore --file-to-restore /db  "${DRIVE_DIR}/${sitename}_backup" ${EE_DIR_PATH}/app/htdocs/db_file
 
 else 
 	echo "Opps! Site not found"
